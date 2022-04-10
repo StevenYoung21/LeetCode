6e367bc545b7ba2b576ff0f52c53926b92f1c1fc
StevenYoung21 <719192406@qq.com>
2022-04-10 22:35:25 +0800

    leetcode 4-10

--- dynamicProgramming/121-买卖股票的最佳时机/2.py
+++ dynamicProgramming/121-买卖股票的最佳时机/2.py
@@ -9,9 +9,7 @@ class Solution:
             if price <= minPrices:
                 minPrices = price
             
-            maxProfit=max(price - minPrices,maxProfit)
-
-
+            maxProfit = max( price - minPrices, maxProfit )
 
         return maxProfit
 
@@ -19,11 +17,14 @@ class Solution:
 """
 思路:
 
-最理想的状态当然是在最低点买入,最高点卖出,但这种情况要保证最高点在最低点的后面.
+最理想的状态当然是在最低点买入, 最高点卖出, 但这种情况要保证最高点在最低点的后面.
 
-如果先在最低点之前就出现了最大值,则需要动态考虑: 我昨天的收益和今天的收益对比一下,哪个更大呢?
+如果先在最低点之前就出现了最大值, 则需要动态考虑: 我昨天的收益和今天的收益对比一下,哪个更大呢?
 
-max
+即: maxProfit = max( price - minPrices, maxProfit )
+
+当天的收益, 就是当天的价格 减去 过去的最低价格, 所以要动态记录出现的最小价格.
+
 
 """
 
