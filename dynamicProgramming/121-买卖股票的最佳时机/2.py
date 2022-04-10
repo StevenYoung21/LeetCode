a07ac42a7c9dd2e8f7ad2cde0eb07de5c046a5a8
StevenYoung21 <719192406@qq.com>
2022-04-10 22:34:26 +0800

    121. 买卖股票的最佳时机

---
+++ dynamicProgramming/121-买卖股票的最佳时机/2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+
+        maxProfit = 0
+
+        minPrices = prices[0]
+
+        for price in prices:
+            if price <= minPrices:
+                minPrices = price
+            
+            maxProfit=max(price - minPrices,maxProfit)
+
+
+
+        return maxProfit
+
+
+"""
+思路:
+
+最理想的状态当然是在最低点买入,最高点卖出,但这种情况要保证最高点在最低点的后面.
+
+如果先在最低点之前就出现了最大值,则需要动态考虑: 我昨天的收益和今天的收益对比一下,哪个更大呢?
+
+max
+
+"""
+
+
+prices = [7,1,5,3,6,4]
+
+s1 = Solution()
+res = s1.maxProfit(prices)
+
+print(res)
