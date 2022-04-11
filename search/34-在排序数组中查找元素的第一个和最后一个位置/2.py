9a269e1785437dddaa8220f006c0916f6b2e1724
StevenYoung21 <719192406@qq.com>
2022-04-11 14:26:36 +0800

    二分查找 左右边界

--- search/34-在排序数组中查找元素的第一个和最后一个位置/2.py
+++ search/34-在排序数组中查找元素的第一个和最后一个位置/2.py
@@ -5,45 +5,52 @@ class Solution:
 
         if len(nums) == 1 and nums[0] == target : return [0,0]
 
-        start = 0
-        end = len(nums) - 1
 
-        while( start <= end ):
-            mid = start + ( end - start ) // 2
-            if nums[mid] > target :
-                end = mid - 1
-            elif nums[mid] < target :
-                start = mid + 1
+        def leftIndex(nums, target):
+
+            start = 0
+            end = len(nums) - 1
+
+            while( start <= end ):
+                mid = start + ( end - start ) // 2
+                if nums[mid] > target :
+                    end = mid - 1
+                elif nums[mid] < target :
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            if nums[start] == target :
+                return start
             else:
-                break
+                return -1
+
+        def rightIndex(nums, target):
+
+            start = 0
+            end = len(nums) - 1
+
+            while( start <= end ):
+                mid = start + ( end - start ) // 2
+                if nums[mid] > target :
+                    end = mid - 1
+                elif nums[mid] < target :
+                    start = mid + 1
+                else:
+                    start = mid + 1
+            if nums[end] == target :
+                return end
+            else:
+                return -1                
+
+        l = leftIndex(nums,target)
+        r = rightIndex(nums, target)
+
+        return [l,r]
 
 
-        l = 0
-        r = len(nums) -1
-
-        res = []
-
-        while( l <= mid or r >= mid ):
-
-            if nums[l] != target and nums[l+1] == target  :
-                res.append(l+1)
-
-            if nums[r] != target and nums[r-1] == target :
-                res.append(r-1)
-            l = l + 1
-            r = r - 1 
-
-        res.sort()
-
-        return res
-
-
-        # return [start,end]
-
-
-nums = [2,2]
+nums = [0]
   
-target = 2
+target = 0
 
 s1 = Solution()
 
