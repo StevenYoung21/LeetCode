287a50f71f029a70b7848030115fa580d9ec7486
StevenYoung21 <719192406@qq.com>
2022-04-27 12:13:26 +0800

    滑动窗口

--- hash/438-找到字符串中所有字母异位词/1.py
+++ hash/438-找到字符串中所有字母异位词/1.py
@@ -1,12 +1,44 @@
+
 class Solution:
     def findAnagrams(self, s: str, p: str) :
+        res = []
 
-        return 1
+        left =  valid = 0
+        right = 0
+        needDic = {}
+
+        for i in range(len(p)):
+            if p[i] not in needDic:
+                needDic[p[i]] = 1
+            else:
+                needDic[p[i]] += 1
+
+        while right < len(s):
+            char = s[right]
+            right += 1
+
+            if char in needDic:
+                needDic[char] -= 1
+                if needDic[char] == 0:
+                    valid += 1
+
+            if right - left == len(p):
+                if valid == len(needDic)  :
+                    res.append(left)
+                char = s[left]
+                left += 1
+
+                if char in needDic:
+                    needDic[char] += 1
+                    if needDic[char] == 1:
+                        valid -= 1              
+
+        return res
 
 
-s = "cbaebabacd"
+s = "abab"
 
-p = "abc"
+p = "ab"
 
 s1 = Solution()
 
