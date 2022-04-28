3219feb6e015ef0f157bc20bc9df6ca06d9f61d4
StevenYoung21 <719192406@qq.com>
2022-04-28 16:05:18 +0800

    哈希表

---
+++ arrary/1-两数之和/4.py
@@ -0,0 +1,27 @@
+
+class Solution:
+    def twoSum(self, nums, target: int) :
+
+        dis = {}
+
+        for index, val in enumerate(nums):
+            if target - val not in dis:
+                dis[val] = index
+            else:
+                return[dis[target-val], index]
+
+
+        return dis
+
+
+
+## 哈希表
+
+nums = [2,7,11,15]
+target = 9
+
+
+s1 = Solution()
+res = s1.twoSum(nums,target)
+
+print(res)
