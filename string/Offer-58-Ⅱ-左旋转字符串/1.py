d8dd85fa9a65be5694ee99602cb292188bdd6b7e
StevenYoung21 <719192406@qq.com>
2022-04-30 00:39:29 +0800

    左旋转字符串

--- string/Offer-58-Ⅱ-左旋转字符串/1.py
+++ string/Offer-58-Ⅱ-左旋转字符串/1.py
@@ -1,15 +1,16 @@
 class Solution:
     def reverseLeftWords(self, s: str, n: int) -> str:
 
-        s1 = s[:-k]
-        s2 = s[-k:]
+        k = n % len(s)
 
+        s1 = s[0:k]
+        s2 = s[k:]
         s3 = s2+s1
 
         return s3
 
 s = "abcdefg"
-k = 2
+k = 9
 
 s1 =Solution()
 print(s1.reverseLeftWords(s,k))
