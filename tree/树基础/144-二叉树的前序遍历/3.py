4a83457c2ca2866ccc8207818d028392818cab38
StevenYoung21 <719192406@qq.com>
2022-05-20 22:44:03 +0800

    迭代法前序遍历二叉树

---
+++ tree/树基础/144-二叉树的前序遍历/3.py
@@ -0,0 +1,42 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root) :
+
+        if not root:
+            return []
+
+        res = []
+
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+
+            res.append(node.val)
+
+            if node.right:
+                stack.append(node.right)
+            
+            if node.left:
+                stack.append(node.left)
+
+        return res
+
+
+root = [1,None,2,3]
+
+c = TreeNode(3,None,None) 
+b = TreeNode(2,c,None)
+a = TreeNode(1,None,b)
+
+root = a
+
+s1 = Solution()
+
+print(s1.preorderTraversal(root))
